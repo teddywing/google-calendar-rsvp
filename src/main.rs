336a4bed16a0b3fda42088e7991537deaf6b5af0
Teddy Wing <git@teddywing.com>
2021-05-23 20:09:17 +0200

    Add brief module-level documentation

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,10 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+//! google-calendar-rsvp
+//!
+//! RSVP to Google Calendar events.
+
 
 use anyhow::{self, Context};
 use base64;
